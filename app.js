4ba1a6bc3a48768dbfb8de856a00c5b1a423ff00
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2022-10-11 00:17:58 -0700

    Formatting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
@@ -25,15 +25,17 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model("User", UserSchema);
 
 // Middleware
-app.set('views', path.join(__dirname, 'views'))
-app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: false,
-	saveUninitialized: true
-}));
+app.set("views", path.join(__dirname, "views"));
+app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -42,97 +44,103 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser(function (user, done) {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-	User.findById(id, function (err, user) {
-		done(err, user);
-	});
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
 });
 
-passport.use(new localStrategy(function (username, password, done) {
-	User.findOne({ username: username }, function (err, user) {
-		if (err) return done(err);
-		if (!user) return done(null, false, { message: 'Incorrect username.' });
+passport.use(
+  new localStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: "Incorrect username." });
 
-		bcrypt.compare(password, user.password, function (err, res) {
-			if (err) return done(err);
-			if (res === false) return done(null, false, { message: 'Incorrect password.' });
-			
-			return done(null, user);
-		});
-	});
-}));
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (err) return done(err);
+        if (res === false)
+          return done(null, false, { message: "Incorrect password." });
+
+        return done(null, user);
+      });
+    });
+  })
+);
 
 function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) return next();
-	res.redirect('/login');
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
 }
 
 function isLoggedOut(req, res, next) {
-	if (!req.isAuthenticated()) return next();
-	res.redirect('/');
+  if (!req.isAuthenticated()) return next();
+  res.redirect("/");
 }
 
 // ROUTES
-app.get('/', isLoggedIn, (req, res) => {
-	res.render("index", { title: "Home" });
+app.get("/", isLoggedIn, (req, res) => {
+  res.render("index", { title: "Home" });
 });
 
-app.get('/about', (req, res) => {
-	res.render("index", { title: "About" });
+app.get("/about", (req, res) => {
+  res.render("index", { title: "About" });
 });
 
-app.get('/login', isLoggedOut, (req, res) => {
-	const response = {
-		title: "Login",
-		error: req.query.error
-	}
+app.get("/login", isLoggedOut, (req, res) => {
+  const response = {
+    title: "Login",
+    error: req.query.error,
+  };
 
-	res.render('login', response);
+  res.render("login", response);
 });
 
-app.post('/login', passport.authenticate('local', {
-	successRedirect: '/',
-	failureRedirect: '/login?error=true'
-}));
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login?error=true",
+  })
+);
 
-app.get('/logout', function(req, res, next) {
-	req.logout(function(err) {
-	  if (err) { 
-		return next(err); 
-		}
-	  res.redirect('/');
-	});
+app.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
+});
 
 // Setup our admin user
-app.get('/setup', async (req, res) => {
-	const exists = await User.exists({ username: "admin" });
+app.get("/setup", async (req, res) => {
+  const exists = await User.exists({ username: "admin" });
 
-	if (exists) {
-		res.redirect('/login');
-		return;
-	};
+  if (exists) {
+    res.redirect("/login");
+    return;
+  }
 
-	bcrypt.genSalt(10, function (err, salt) {
-		if (err) return next(err);
-		bcrypt.hash("pass", salt, function (err, hash) {
-			if (err) return next(err);
-			
-			const newAdmin = new User({
-				username: "admin",
-				password: hash
-			});
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash("pass", salt, function (err, hash) {
+      if (err) return next(err);
 
-			newAdmin.save();
+      const newAdmin = new User({
+        username: "admin",
+        password: hash,
+      });
 
-			res.redirect('/login');
-		});
-	});
+      newAdmin.save();
+
+      res.redirect("/login");
+    });
+  });
 });
 
 app.listen(3000, () => {
-	console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
